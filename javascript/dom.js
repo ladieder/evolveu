f772cfe2370cc1c3d7d9328e986c2d2c44472d32
ladieder <48527775+ladieder@users.noreply.github.com>
2019-03-28 14:00:53 -0600

    update

--- javascript/dom.js
+++ javascript/dom.js
@@ -10,6 +10,8 @@ function onShowClick() {
 	console.log("in onShowClick");
 	let showAr = [];
 
+// ??? i+2 blew it up so had to do i++ twice ???
+
 	for (let i = 1; i < oList.childNodes.length; i++) {
 		showAr.push(oList.childNodes[i].textContent);
 		console.log("showAr", showAr);
@@ -45,11 +47,48 @@ endBtn.addEventListener("click", onEndClick);
 oList.addEventListener("click", onDeleteClick);
 
 
+
 // *** Exercise - Working with Cards ***
+
 console.log("in dom.js cards exercise");
 
-function onAddClick0() {
-	console.log("in onAddClick0");
+let counter = 0;
+
+function createCard() {
+	console.log("in createCard");
+	const div = document.createElement("div");
+	div.textContent = "Card " + ++counter;
+	div.className = "card"
+	return div;
 }
 
-addBtn0.addEventListener("click", onAddClick0);
+function createBtn(btnName, btnClass) {
+	console.log("in createBtn");
+	const btn = document.createElement("button");
+	btn.textContent = btnName;
+	btn.className = btnClass;
+	return btn;
+}
+
+function addBeforeFunc() {
+
+}
+
+function addAfterFunc() {
+	
+}
+
+function deleteFunc() {
+	
+}
+
+function onAddClick() {
+	console.log("in onAddClick");
+	const newDiv = createCard();
+	cards.append(newDiv);
+	newDiv.appendChild(createBtn("Add Before", "beforeBtn"));
+	newDiv.appendChild(createBtn("Add After", "afterBtn"));
+	newDiv.appendChild(createBtn("Delete", "deleteBtn"));	
+}
+
+addCardBtn.addEventListener("click", onAddClick);
