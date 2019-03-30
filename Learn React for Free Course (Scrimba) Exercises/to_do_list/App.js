b7307926182e09dc1f197f1e3a0a78001cf3afb0
ladieder <48527775+ladieder@users.noreply.github.com>
2019-03-30 17:00:51 -0600

    march 30 update

---
+++ Learn React for Free Course (Scrimba) Exercises/to_do_list/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function App() {
+    return(
+        <div>
+            <input type="checkbox" />
+            <p>Laundry</p>
+
+            <input type="checkbox" />
+            <p>Dishes</p>
+
+            <input type="checkbox" />
+            <p>Tidy room</p>
+
+        </div>
+    )
+}
+
+export default App
