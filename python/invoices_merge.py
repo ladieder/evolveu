b6c3f1d17a9aced7db27a854793d0163f8fad00c
ladieder <48527775+ladieder@users.noreply.github.com>
2019-05-21 10:25:30 -0600

    may 21 update

--- python/invoices_merge.py
+++ python/invoices_merge.py
@@ -1,19 +1,38 @@
 from openpyxl import load_workbook
+from invoices_validation import invoices_validation
 
-wbmerge = load_workbook('invoices.xlsx')
-wbmaster = load_workbook('invoices_master.xlsx')
+# wbmerge = load_workbook('invoices.xlsx')
+# wbmaster = load_workbook('invoices_master.xlsx')
+
+def invoices_merge(wb_merge):
+    dict_merge = invoices_validation(wb_merge)
+    dict_master = invoices_validation('invoices_master.xlsx')
+
+    print(dict_merge)
+    print(dict_master)
+
+    if dict_master["sheet_names"] != dict_merge["sheet_names"]:
+        print("error - workbook sheet names do not match")
+    else:
+        pass
+
+        # *** remember to filter out duplicates
+
+        # merge customers
+
+        # merge products
+
+        # merge invoices
+
+
+invoices_merge('invoices.xlsx')
 
 
 
+# l = [[1, 2, 3], [2, 4, 5], [1, 2, 3], [2, 4, 5]]
+# # s = set(tuple(i) for i in l)
+# s = set()
+# for i in l:
+#     s.add(tuple(i))
 
-
-
-
-
-l = [[1, 2, 3], [2, 4, 5], [1, 2, 3], [2, 4, 5]]
-# s = set(tuple(i) for i in l)
-s = set()
-for i in l:
-    s.add(tuple(i))
-
-print(s)
+# print(s)
